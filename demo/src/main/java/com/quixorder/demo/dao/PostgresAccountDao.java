341bd208e5376d54b692d9acf449adce5ef48d85
John Ta <john.ta@student.csulb.edu>
2020-02-14 06:03:35 -0800

    Completed postgresaccountdao.java
    Executes postgresql queries

--- demo/src/main/java/com/quixorder/demo/dao/PostgresAccountDao.java
+++ demo/src/main/java/com/quixorder/demo/dao/PostgresAccountDao.java
@@ -1,4 +1,99 @@
 package com.quixorder.demo.dao;
 
-public class PostgresAccountDao {
+import com.quixorder.demo.model.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * The PostgresAccountDao is an AccountDao subclass that uses sql commands to fulfill PostgreSQL database requests
+ */
+@Repository("postgres")
+public class PostgresAccountDao implements AccountDao{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PostgresAccountDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    /**
+     * The function insertAccount(id, account) adds a new entry into the account table
+     * @param id This is the primary key for account that is being added
+     * @param account This holds all other values for account
+     * @return
+     */
+    @Override
+    public int insertAccount(UUID id, Account account) {
+        final String sql = "INSERT INTO account (id, type, username, password) VALUES(?, ?, ?, ?)";
+        String type = account.getType();
+        String username = account.getUsername();
+        String password = account.getPassword();
+        return jdbcTemplate.update(sql, id, type, username, password);
+    }
+
+    /**
+     * The function selectAllAccount() selects all accounts and returns it
+     * @return a list of all account objects
+     */
+    @Override
+    public List<Account> selectAllAccount() {
+        final String sql = "SELECT * FROM account";
+        return jdbcTemplate.query(sql, (resultSet, i) -> {
+           UUID id = UUID.fromString(resultSet.getString("id"));
+           String type = resultSet.getString("type");
+           String username = resultSet.getString("username");
+           String password = resultSet.getString("password");
+           return new Account(id, type, username, password);
+        });
+    }
+
+    /**
+     * The function selectAccountById() searches for an account and returns it
+     * @param id This is the primary key for account that is being searched for
+     * @return the entry whose ID matches
+     */
+    @Override
+    public Optional<Account> selectAccountById(UUID id) {
+        final String sql = "SELECT * FROM account WHERE id = ?";
+        Account account = jdbcTemplate.queryForObject(sql, new Object[]{id}, (resultSet, i) -> {
+            UUID accountId = UUID.fromString(resultSet.getString("id"));
+            String type = resultSet.getString("type");
+            String username = resultSet.getString("username");
+            String password = resultSet.getString("password");
+            return new Account(id, type, username, password);
+        });
+        return Optional.ofNullable(account);
+    }
+
+    /**
+     * The function deleteAccountById() searches and deletes an account
+     * @param id This is the primary key for account that is being deleted
+     * @return the number of entries deleted
+     */
+    @Override
+    public int deleteAccountById(UUID id) {
+        final String sql = "DELETE FROM account WHERE id = ?";
+        return jdbcTemplate.update(sql, id);
+    }
+
+    /**
+     * The function updateAccountById() searches and updates an account with new information
+     * @param id This is the primary key for account that is being updated
+     * @param account This holds the information to update with
+     * @return the number of entries updated
+     */
+    @Override
+    public int updateAccountById(UUID id, Account account) {
+        final String sql = "UPDATE account SET type = ?, username = ?, password = ? WHERE id = ?";
+        String type = account.getType();
+        String username = account.getUsername();
+        String password = account.getPassword();
+        return jdbcTemplate.update(sql, type, username, password, id);
+    }
 }
--- demo/src/main/resources/db/migration/V1__AccountTable.sql
+++ demo/src/main/resources/db/migration/V1__AccountTable.sql
@@ -2,5 +2,6 @@ CREATE TABLE account (
   id UUID NOT NULL PRIMARY KEY,
   type VARCHAR(6) NOT NULL,
   username VARCHAR(50) NOT NULL,
-  password VARCHAR(32) NOT NULL
+  password VARCHAR(32) NOT NULL,
+  CONSTRAINT uc_account UNIQUE (type, username)
 );
